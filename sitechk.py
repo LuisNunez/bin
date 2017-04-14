dfa97ea8bd9db9a7e47923c54d6092cd8d259a7e
luisnunez <lknunez@gmail.com>
2017-04-14 10:53:42 -0400

    add main() and argv 	modified:   sitechk.py

--- sitechk.py
+++ sitechk.py
@@ -1,41 +1,48 @@
 # script to test website status
 # looking for status 200
+__author__ = "Luis Nunez"
+__license__ = "GPLv3"
+__version__ = "0.1.1"
+__maintainer__ = "Luis Nunez"
+__status__ = "Prototype"
 
+
+from sys import argv
 import requests
 import socket
 
 
-version = '0.1.1'
 
-print 'Version:', version
+print 'Version:', __version__
 
-
-UrlFile = open('file.txt')
 
 # Colors
 Red = '\033[91m'
 CEnd = '\033[0m'
 
-for Url in UrlFile:
-    #print Url
-    #lenght = len(Url)
-    # Remove ending character
-    x = Url.strip()
-    #print x
-    try:
-        Status = requests.get(x)
-        fqdn = socket.getfqdn(x.replace('https://',''))
-        aka = socket.gethostbyname_ex(x.replace('https://',''))
-        code = Status.status_code
-        code_str = str(code)
-        if code_str != '200':
-            prn_code_str= '\33[5m'+Red + code_str + '\33[6m'+CEnd
-            print prn_code_str, x, fqdn, aka[2]
-        else:
-            prn_code_str = code_str
-            print prn_code_str, x, fqdn, aka[2]
-    except:
-        print Red + "Exception" + CEnd ,x
+def main(File):
+    UrlFile = open(File)
+    for Url in UrlFile:
+        #print Url
+        #lenght = len(Url)
+        # Remove ending character
+        x = Url.strip()
+        #print x
+        try:
+            Status = requests.get(x)
+            fqdn = socket.getfqdn(x.replace('https://',''))
+            aka = socket.gethostbyname_ex(x.replace('https://',''))
+            code = Status.status_code
+            code_str = str(code)
+            if code_str != '200':
+                prn_code_str= '\33[5m'+Red + code_str + '\33[6m'+CEnd
+                print prn_code_str, x, fqdn, aka[2]
+            else:
+                prn_code_str = code_str
+                print prn_code_str, x, fqdn, aka[2]
+        except:
+            print Red + "Exception" + CEnd ,x
 
-
+if __name__ == '__main__':
+    main(argv[1])
 
