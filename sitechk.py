4ed9f77df6fe4d9e863a4e75ec62a7c883f5f5ae
luisnunez <lknunez@gmail.com>
2017-04-15 08:11:34 -0400

    update variables 	modified:   sitechk.py

--- sitechk.py
+++ sitechk.py
@@ -19,6 +19,8 @@ print 'Version:', __version__
 # Colors
 Red = '\033[91m'
 CEnd = '\033[0m'
+Blink = '\33[5m'
+BlinkEnd = '\33[6m'
 
 def main(File):
     UrlFile = open(File)
@@ -35,7 +37,8 @@ def main(File):
             code = Status.status_code
             code_str = str(code)
             if code_str != '200':
-                prn_code_str= '\33[5m'+Red + code_str + '\33[6m'+CEnd
+                #prn_code_str= '\33[5m'+Red + code_str + '\33[6m'+CEnd
+                prn_code_str= Blink +Red + code_str + BlinkEnd + CEnd
                 print prn_code_str, x, fqdn, aka[2]
             else:
                 prn_code_str = code_str
