7d444854472f7203206c64f7d01483c1fc6b4b92
luisnunez <lknunez@gmail.com>
2017-04-16 16:30:50 -0400

    pep8 and add /n in execption

--- sitechk.py
+++ sitechk.py
@@ -1,17 +1,15 @@
 # script to test website status
 # looking for status 200
-__author__ = "Luis Nunez"
-__license__ = "GPLv3"
-__version__ = "0.1.1"
-__maintainer__ = "Luis Nunez"
-__status__ = "Prototype"
-
-
 from sys import argv
 import requests
 import socket
 import sys
 
+__author__ = "Luis Nunez"
+__license__ = "GPLv3"
+__version__ = "0.1.1"
+__maintainer__ = "Luis Nunez"
+__status__ = "Prototype"
 
 
 print 'Version:', __version__
@@ -23,31 +21,26 @@ CEnd = '\033[0m'
 Blink = '\33[5m'
 BlinkEnd = '\33[6m'
 
+
 def main(File):
     UrlFile = open(File)
     for Url in UrlFile:
-        #print Url
-        #lenght = len(Url)
-        # Remove ending character
         x = Url.strip()
-        #print x
         try:
             Status = requests.get(x)
-            fqdn = socket.getfqdn(x.replace('https://',''))
-            aka = socket.gethostbyname_ex(x.replace('https://',''))
+            fqdn = socket.getfqdn(x.replace('https://', ''))
+            aka = socket.gethostbyname_ex(x.replace('https://', ''))
             code = Status.status_code
             code_str = str(code)
             if code_str != '200':
-                #prn_code_str= '\33[5m'+Red + code_str + '\33[6m'+CEnd
-                prn_code_str= Blink +Red + code_str + BlinkEnd + CEnd
+                prn_code_str = Blink + Red + code_str + BlinkEnd + CEnd
                 print prn_code_str, x, fqdn, aka[2]
             else:
                 prn_code_str = code_str
                 print prn_code_str, x, fqdn, aka[2]
         except Exception, Error:
-            print Red + "Exception" + CEnd ,x
-            sys.stderr.write('ERROR: %sn' % str(Error))
+            print Red + "Exception" + CEnd, x
+            sys.stderr.write('ERROR: %s\n' % str(Error))
 
 if __name__ == '__main__':
     main(argv[1])
-
