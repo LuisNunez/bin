a59676b7dbbd4b3d3af3d1f1987747e72b304a89
luisnunez <lknunez@gmail.com>
2017-04-19 14:35:22 -0400

    add sample test file 	modified:   sitechk.py 	new file:   testfile.csv

--- sitechk.py
+++ sitechk.py
@@ -30,7 +30,7 @@ BlinkEnd = '\33[6m'
 def main(File):
     UrlFile = csv.DictReader(open(File,'rb'))
     for Url in UrlFile:
-        x = Url['ssl'] + Url['host'].strip() + Url['path'].strip()
+        x = Url['proto'] + Url['host'].strip() + Url['path'].strip()
         #print(Url['host'])
         #print "URL: ", x
         try:
